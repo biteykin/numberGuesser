616d8a258664f5c61c9bdbdeb48481e44da1fe1e
biteykin <ibiteykin@gmail.com>
2020-04-02 21:51:53 +0300

    v.1.0.0

--- script.js
+++ script.js
@@ -4,7 +4,34 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const generateTarget = () => {
 
+//determine the target number
+const generateTarget = () => {
+return Math.floor(Math.random() * 10);
 }
 
+
+//determine the round winner
+const compareGuesses = (humanGuessInput, computerGuess, target) => {
+    if (Math.abs(target - humanGuessInput) <= Math.abs(target - computerGuess)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// update the display score
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        return humanScore++;
+    } else {
+        return computerScore++;
+    }
+}
+
+
+//update round number
+const advanceRound = () => {
+ return currentRoundNumber++;
+}
